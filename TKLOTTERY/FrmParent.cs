f9bc4d691ff3aa2fa3e921dea8cac47c8f9b6387
at3158 <tk290@tkfood.com.tw>
2021-10-06 09:34:51 +0800

    GetHostIPAddress

--- TKLOTTERY/FrmParent.cs
+++ TKLOTTERY/FrmParent.cs
@@ -11,6 +11,8 @@ using System.Configuration;
 using System.Reflection;
 using System.Diagnostics;
 using TKITDLL;
+using System.Net;
+using System.Net.Sockets;
 
 namespace TKLOTTERY
 {
@@ -98,7 +100,9 @@ namespace TKLOTTERY
             datransaction.Fill(dtransaction);
 
             //ADD USED LOG
-            TKSYSPRUSED(MethodBase.GetCurrentMethod().DeclaringType.Namespace, dtransaction.Rows[0]["FRM_CODE"].ToString(), sender.ToString(), UserName);
+            List<string> IPAddress = GetHostIPAddress();
+            //MessageBox.Show(IPAddress[0].ToString());            
+            TKSYSPRUSED(MethodBase.GetCurrentMethod().DeclaringType.Namespace, dtransaction.Rows[0]["FRM_CODE"].ToString(), sender.ToString(), UserName, IPAddress[0].ToString());
 
 
             Assembly frmAssembly = Assembly.LoadFile(Application.ExecutablePath);
@@ -136,7 +140,7 @@ namespace TKLOTTERY
 
         }
 
-        public void TKSYSPRUSED(string SYSTEMNAME, string PROGRAMCODE, string PROGRAMNAME, string USEDID)
+        public void TKSYSPRUSED(string SYSTEMNAME, string PROGRAMCODE, string PROGRAMNAME, string USEDID, string USEDIP)
         {
             SqlConnection sqlConn = new SqlConnection();
             SqlTransaction tran;
@@ -167,9 +171,9 @@ namespace TKLOTTERY
 
             sbSql.AppendFormat(@" 
                                 INSERT INTO [TKIT].[dbo].[TKSYSPRUSED]
-                                ([SYSTEMNAME],[PROGRAMCODE],[PROGRAMNAME],[USEDDATES],[USEDID])
+                                ([SYSTEMNAME],[PROGRAMCODE],[PROGRAMNAME],[USEDDATES],[USEDID],[USEDIP])
                                 VALUES
-                                (@SYSTEMNAME,@PROGRAMCODE,@PROGRAMNAME,@USEDDATES,@USEDID)
+                                (@SYSTEMNAME,@PROGRAMCODE,@PROGRAMNAME,@USEDDATES,@USEDID,@USEDIP)
                                 ");
 
 
@@ -181,6 +185,7 @@ namespace TKLOTTERY
                 command.Parameters.AddWithValue("@PROGRAMNAME", PROGRAMNAME);
                 command.Parameters.AddWithValue("@USEDDATES", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
                 command.Parameters.AddWithValue("@USEDID", USEDID);
+                command.Parameters.AddWithValue("@USEDIP", USEDIP);
 
                 try
                 {
@@ -200,6 +205,26 @@ namespace TKLOTTERY
             }
 
 
+        }
+
+        // <summary>
+        /// 取得本機 IP Address
+        /// </summary>
+        /// <returns></returns>
+        private List<string> GetHostIPAddress()
+        {
+            List<string> lstIPAddress = new List<string>();
+            IPHostEntry IpEntry = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ipa in IpEntry.AddressList)
+            {
+                if (ipa.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    lstIPAddress.Add(ipa.ToString());
+                    //MessageBox.Show(ipa.ToString());
+                }
+
+            }
+            return lstIPAddress; // result: 192.168.1.17 ......
         }
     }
 }
